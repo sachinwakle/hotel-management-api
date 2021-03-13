d5f8f9df01b4916d8b94a747e0502d3172d1f4c9
sachinwakle <sbwakle@hotmail.com>
2021-03-13 12:21:12 +0530

    initial setup of API service

---
+++ src/test/java/com/hotel/app/HotelManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
